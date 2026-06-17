c4e46b882ae70ca7e28b380d0429bf8fb7907f3e
MarceloAnsilago <celosmg1@gmail.com>
2026-06-17 13:00:48 -0400

    Ajusta largura da restituicao

--- views/restituicao.py
+++ views/restituicao.py
@@ -193,7 +193,7 @@ def build_pdf_restituicao(data: dict, logo_path: Path) -> bytes:
     return buffer.read()
 
 def render_restituicao():
-    col_esq, col_meio, col_dir = st.columns([1, 2, 1])
+    col_esq, col_meio, col_dir = st.columns([0.1, 8, 0.1])
     with col_meio:
         st.markdown("## Requerimento de restituição de valor recolhido indevidamente")
         with st.form("form_restituicao_inicial"):
